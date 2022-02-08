7447e017515112f5b1b953190b1ad5ae13462c2e
wonjuneseo <wonjuneprog@gmail.com>
2022-02-08 20:17:58 +0900

    Update RBGcolor.cpp

--- RBGcolor.cpp
+++ RBGcolor.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <Write.h>
 
+//RGB sensor pin settings
+#define redpin = 13;
+#define greenpin = 12;
+#define bluepin = 11;
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725
 
 void setup() {
   // put your setup code here, to run once:
